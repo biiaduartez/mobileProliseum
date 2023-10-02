c58910aa89aef914929226b8b4f3e8f24c4adc43
Bianca Duarte <123369796+biiaduartez@users.noreply.github.com>
2023-10-02 08:39:57 -0300

    menu funcional

--- app/src/main/java/com/example/mobileproliseum/MainActivity.kt
+++ app/src/main/java/com/example/mobileproliseum/MainActivity.kt
@@ -1,85 +1,28 @@
 package com.example.mobileproliseum
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.drawscope.translate
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import com.example.mobileproliseum.ui.theme.AzulEscuroProliseum
-import com.example.mobileproliseum.ui.theme.BlackTransparentProliseum
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.senai.sp.jandira.mobileproliseum.home.screen.HomeScreen
 import com.example.mobileproliseum.ui.theme.MobileProliseumTheme
 import com.example.mobileproliseum.ui.theme.RedProliseum
-import com.example.mobileproliseum.ui.theme.WhiteTransparentProliseum
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -91,61 +34,66 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "menu") {
+                        composable("menu") {
+                            Greeting(navController = navController)
+                        }
+                        composable("home") {
+                            HomeScreen() // Certifique-se de passar os parâmetros necessários, se houver
+                        }
+                    }
                 }
             }
         }
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(navController: NavController, modifier: Modifier = Modifier) {
     var isMenuVisible by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
+        // Conteúdo do menu que aparecerá quando isMenuVisible for verdadeiro
         if (isMenuVisible) {
-            // Conteúdo do seu menu aqui
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(
                         brush = Brush.verticalGradient(
-                            colors = listOf(AzulEscuroProliseum, AzulEscuroProliseum), // Duas vezes para garantir a cor sólida
+                            colors = listOf(RedProliseum, RedProliseum),
                         )
                     )
-                    .clickable { isMenuVisible = false }
             ) {
-                // Conteúdo do item do menu
+                // Conteúdo do item do menu com ação de navegação
+                Text(
+                    text = "Home",
+                    modifier = Modifier
+                        .padding(16.dp, top = 55.dp)
+                        .clickable {
+                            navController.navigate("home") // Navega para a rota "home"
+                        },
+                    color = Color.White
+                )
             }
         }
 
+        // IconButton que permanecerá visível na parte superior
         IconButton(
             onClick = { isMenuVisible = !isMenuVisible },
             modifier = Modifier
-                .size(75.dp)
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(AzulEscuroProliseum, AzulEscuroProliseum), // Duas vezes para garantir a cor sólida
-                    )
-                )
+                .size(48.dp)
+                .background(MaterialTheme.colorScheme.primary)
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.logocadastro),
                 contentDescription = "Menu Icon",
-                tint = if (isMenuVisible) Color.White else Color.Red
+                tint = if (isMenuVisible) Color.White else Color.Black
             )
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileProliseumTheme {
-        Greeting("Android")
     }
 }
